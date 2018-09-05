964d931855429c814b029fa0a77c1bf2b09921b5
Richard Erickson <11819537+rerickson-usgs@users.noreply.github.com>
2018-09-05 09:42:47 -0500

    added documentation to the code

--- coefModel/simSampleSize.R
+++ coefModel/simSampleSize.R
@@ -9,6 +9,11 @@ pPositivePlot <- fread("pPositivePlot.csv")
 
 
 ## Define custom functions
+
+## this function calculates the probability of 
+## detecting eDNA in at least one sample
+## over a range of sample sizes given 
+## a detection probability pPos
 calcDetectOne <- function(minSample = 1,
                           maxSample = 1,
                           pPos = 0.5){
@@ -18,12 +23,19 @@ calcDetectOne <- function(minSample = 1,
 }
 calcDetectOne(maxSample = 4)
 
-
+## This function takes a vector, x, and finds the nearst value 
+## to "desiredProb". It is largely an internal function used later.
 nearestValue <- function(desiredProb = 1/3, x = seq(0,1, by = 0.25)){
     which(abs(x-desiredProb)==min(abs(x-desiredProb)))
 }
 nearestValue()
 
+## this calculates the the number of samples needed 
+## to get one detection given a desired detection probability
+## it requires a minimum and maximum number of samples,
+## a desired detection probability
+## and a vector of possible detection probabilities
+## This is largely an internal function.
 samplesForOne <- function(minSample = 1, maxSample = 1000,
                           desiredProb = 0.8,
                           pOne = seq(0, 1, length.out = 1000)){
@@ -32,6 +44,10 @@ samplesForOne <- function(minSample = 1, maxSample = 1000,
 }
 samplesForOne()
 
+## This is a wrapper funciton that calcluates the sample size 
+## needed to detect eDNA in at least one sample given 
+## a desired detection probability, a min and max number of possible samples
+## and a probability of positive detection 
 sampleSizeCalc <- function(minSample = 1,
                            maxSample = 250,
                            desiredProb = 0.8,
@@ -45,6 +61,9 @@ sampleSizeCalc <- function(minSample = 1,
 }
 sampleSizeCalc()
 
+## This is a wrapper funciton for the USFWS UMR monitoring data. 
+## it requires input from the fitted model (currently not publicly available).
+## It takes the same inputs as above, but uses a specific sampling month and site. 
 siteMonthWrapper <- function(month = "November",
                              site = "Boston Bay backwater",
                              data = pPositivePlot,
